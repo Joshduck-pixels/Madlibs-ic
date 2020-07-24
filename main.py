3987dd8d13a024b8569cdf32ccfe521dd89ba56a
joshualau <joshualau2007@gmail.com>
2020-07-24 18:18:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+name = input("Hello, what is your name? \n")
+print(f"Hello {name}! Let's start.")
+#print("Hello " + name + "! Let's start.")
+
+print()
+
+holiday = input("Enter a name of a holiday: ")
+place = input("Enter a place: ")
+hero = input('Enter a name of a hero: ')
+outfit = input("What would you like to wear: ")
+power = input("What super powers would you like to have?")
+favorite_number = input("What is your favorite number: ")
+animals = input("What is your favorite animal: ")
+app = input("Enter your favorite app: ")
+tv_show = input("What is your favorite tv show: ")
+
+print(f"Everyday at noon the farmer will attend to his {favorite_number} {animals}. After attending to his {animals} he scrolls through his phone and opens {app}. Then he turns on the tv and re-enacts his favorite scene from {tv_show}. He starts running around {place} with his {power}. ")
+
